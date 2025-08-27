87b29192c50b99d353f4583cb84b855919115d7d
ferron321 <ast@webizi.ru>
2025-08-27 11:39:24 +0300

    New one task

--- ai.c
+++ ai.c
@@ -6,7 +6,7 @@ int j;
 int sum = 0;
 for( int i=0; i<=j;j++)
 {
-   for ( j = 0; j <=i ; i++)
+   for (j = 0; j <=i ; i++)
    {
     
    }
---
+++ yar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+int a = 5;
+int b = 10;
+int c = 0;
+c = a;
+a = b;
+b = c;
+
+printf( "a= %d b= %d",a,b);
+
+
+
+}
+
